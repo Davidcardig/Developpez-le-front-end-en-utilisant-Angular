aaebf416816ef008a0706484a4aa2ef6726e6cc0
Davidcardig <davidcardigos@hotmail.fr>
2025-09-19 20:17:07 +0200

    Ajout des dépendances Angular CDK et ngx-charts ; mise à jour du composant Home pour l’affichage du graphique

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,18 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, of } from 'rxjs';
 import { OlympicService } from 'src/app/core/services/olympic.service';
+import { Observable, of } from 'rxjs';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss'],
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  chartData: any[] = [];
   public olympics$: Observable<any> = of(null);
+  numberOfJOs: number = 0;
+  numberOfCountries: number = 0;
 
   constructor(private olympicService: OlympicService) {}
 
   ngOnInit(): void {
-    this.olympics$ = this.olympicService.getOlympics();
+    this.olympicService.getOlympics().subscribe(data => {
+      this.getOlympicsData();
+    });
   }
+
+  public getOlympicsData(): void {
+    this.olympicService.getOlympics().subscribe(data => {
+      const olympics = data ?? [];
+      this.chartData = olympics.map(item => ({
+        name: item.country,
+        value: item.participations.reduce((total, p) => total + (p.medalsCount || 0), 0)
+      }));
+    
+    });
+
+  }
+
+  
 }
